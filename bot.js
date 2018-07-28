e493d685b8b6ec7dab9649b4b927a077b392513e
iSmilex <38261980+iSmilex@users.noreply.github.com>
2018-07-29 00:21:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -287,10 +287,10 @@ ${text}
 
 // القبول - الرفض
 
- client.on('message',async message => {
+  client.on('message',async message => {
   let mention = message.mentions.members.first();
   let role = message.content.split(" ").slice(2).join(" ");
-  let mySupport = message.guild.roles.find('name', role);
+  let mySupport = message.guild.roles.find('name',role);
   let acRoom = client.channels.get('472887703411752960');
   if(message.content.startsWith(prefix + "accept")) {
     if(message.guild.id !== '468167578855014411') return;
@@ -299,10 +299,13 @@ ${text}
     if(!role) return message.reply('ادخل اسم رتبة');
     if(!mySupport) return message.reply('هذه الرتبة غير موجودة');
     if(mention.roles.has(mySupport)) return message.reply('هذا الشخص معه الرتبة مسبقا');
-    message.guild.member(user).addRole(mySupport);
-     await acRoom.send(`**[ ${mySupport} ] واعطائك رتبة ${mention} تم بنجاح قبولك**`);
+
+    mention.addRole(mySupport).then(() => {
+      acRoom.send(`**[ ${mySupport} ] واعطائك رتبة ${mention} تم بنجاح قبولك**`);
+    });
   }
 });
+  
 
 
 client.login(process.env.BOT_TOKEN);

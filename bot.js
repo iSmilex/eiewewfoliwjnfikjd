972a5967343246b0deb4b0df5b51a42d3ed9fc7f
iSmilex <38261980+iSmilex@users.noreply.github.com>
2018-08-31 02:40:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,7 +63,6 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.s
 // Ultimate
  client.on('message', message => {
               if(!message.channel.guild) return;
-    var prefix = "$";
     if(message.content.startsWith('$bc')) {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
